cf8aef16371face9d2d89393ec7f1ff203926761
kananbayramli <bayramli-kenan@mail.ru>
2024-04-20 20:34:08 +0400

    First commit

---
+++ Category.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace shopapp.entity
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public List<ProductCategory> ProductCategory { get; set; }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace shopapp.entity
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set;}
+        public decimal? Price { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public bool IsApproved { get; set; }
+        public List<ProductCategory> ProductCategory { get; set; }
+    }
+}
