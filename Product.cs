85fa42bd8673d4ad11f09282c71544ee1e5b3fa8
kananbayramli <bayramli-kenan@mail.ru>
2024-04-25 15:54:27 +0400

    Prodcuts link

--- Product.cs
+++ Product.cs
@@ -6,6 +6,7 @@ namespace shopapp.entity
     {
         public int ProductId { get; set; }
         public string Name { get; set;}
+        public string Url { get; set;}
         public decimal? Price { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
