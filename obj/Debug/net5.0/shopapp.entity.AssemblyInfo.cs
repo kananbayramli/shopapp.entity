3649d03cdfe418c2e425138eeffffd7b1c7babc4
kananbayramli <bayramli-kenan@mail.ru>
2024-05-10 18:48:58 +0400

    AddToBasket Configuring

--- obj/Debug/net5.0/shopapp.entity.AssemblyInfo.cs
+++ obj/Debug/net5.0/shopapp.entity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shopapp.entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fc41b1e598a4088045a2d54f5c447e39b5c0efc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfbd8263ec637e79b95b7b5acc1a4de888298f5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("shopapp.entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shopapp.entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
