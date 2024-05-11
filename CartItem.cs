fafe97e1195475ca36f1178caf3fbdfc93a6d8e1
kananbayramli <bayramli-kenan@mail.ru>
2024-05-11 18:53:59 +0400

    Obtaining Shopping Cart Information

--- CartItem.cs
+++ CartItem.cs
@@ -4,10 +4,10 @@ namespace shopapp.entity
     {
         public int Id { get; set; }
 
-        public string CartId { get; set; }
+        public int CartId { get; set; }
         public Cart Cart { get; set; }
 
-        public string ProductId { get; set; }
+        public int ProductId { get; set; }
         public Product Product { get; set; }
         
         public int Quantity { get; set; }
