f05d49de8cb9418fe370e27aef64741ca8d0bf9e
kananbayramli <bayramli-kenan@mail.ru>
2024-05-12 15:30:49 +0400

    AddOrderAndOrderItemsEntities

--- Order.cs
+++ Order.cs
@@ -1,7 +1,31 @@
+using System;
+using System.Collections.Generic;
+
 namespace shopapp.entity
 {
+    public enum EnumOrderState
+    {
+        waiting = 0,
+        unpaid = 1,
+        completed = 2
+    }
     public class Order
     {
-        public int OrderId { get; set; }
+        public int Id { get; set; }
+        public string OrderNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string UserId { get; set; }
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Note { get; set; }
+
+        public EnumOrderState OrderState { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
     }
 }
